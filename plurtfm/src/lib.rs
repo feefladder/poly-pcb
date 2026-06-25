a548430922ad21d4295858b6d685a45a496e3b73
Fee Fladder <freakyfee@proton.me>
2026-06-25 16:30:35 +0200

    I have no idea what I did

--- plurtfm/src/lib.rs
+++ plurtfm/src/lib.rs
@@ -1,6 +1,22 @@
 use rusqlite::{Connection, Result};
-use std::error::Error;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, Response, console::log_1, window};
+
+// need to fetch db from site
+// use bare api: https://rustwasm.app/en/learn/fetch-api
+async fn fetch_db(path: &str) -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.set_method("GET");
+
+    let request = Request::new_with_str_and_init(path, &opts)?;
+    let window = window().unwrap();
+    let resp: Response = JsFuture::from(window.fetch_with_request(&request))
+        .await?
+        .dyn_into()?;
+
+    Ok(resp.array_buffer()?.await?)
+}
 
 #[derive(Debug)]
 struct Person {
@@ -24,52 +40,34 @@ impl From<ErrorShim> for JsValue {
 }
 
 #[wasm_bindgen]
-pub fn run_sqlite_demo() -> Result<(), JsValue> {
+pub async fn run_sqlite_demo() -> Result<(), JsValue> {
     // Set up panic hook for better error messages in the browser
     console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Trace).unwrap();
 
+    let db_bytes = fetch_db("polydb.sqlite3")
+        .await
+        .map_err(|e| ErrorShim::from(e))?;
     // Open an in-memory database
     let conn = Connection::open_in_memory().map_err(|e| ErrorShim::from(e))?;
-
-    conn.execute(
-        "CREATE TABLE person (
-            id    INTEGER PRIMARY KEY,
-            name  TEXT NOT NULL,
-            data  BLOB
-        )",
-        (),
-    )
-    .map_err(|e| ErrorShim::from(e))?;
-
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        data: None,
-    };
-
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        (&me.name, &me.data),
-    )
-    .map_err(|e| ErrorShim::from(e))?;
+    conn.restore_from_memory(&db_bytes);
 
     let mut stmt = conn
-        .prepare("SELECT id, name, data FROM person")
+        .prepare(
+            "SELECT
+        name
+    FROM
+        sqlite_schema
+    WHERE
+        type ='table' AND
+        name NOT LIKE 'sqlite_%';",
+        )
         .map_err(|e| ErrorShim::from(e))?;
-    let person_iter = stmt
-        .query_map([], |row| {
-            Ok(Person {
-                id: row.get(0)?,
-                name: row.get(1)?,
-                data: row.get(2)?,
-            })
-        })
+    let res = stmt
+        .query_map([], |row| Ok(format!("{row:?}")))
         .map_err(|e| ErrorShim::from(e))?;
-
-    for person in person_iter {
-        web_sys::console::log_1(&format!("Found person {:?}", person.unwrap()).into());
+    for row in res {
+        log_1(&JsValue::from_str(&row.unwrap()));
     }
-
     Ok(())
 }
